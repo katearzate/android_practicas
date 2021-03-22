ed83fd6f343cbfbca36719eac6962d97fd3d457f
katearzate <katearzate000@gmail.com>
2021-03-22 16:16:52 -0700

    Agregar imagen al segundo activity

--- TaquillaCine/app/src/main/java/com/example/taquillacine/CompraActivity.kt
+++ TaquillaCine/app/src/main/java/com/example/taquillacine/CompraActivity.kt
@@ -2,6 +2,7 @@ package com.example.taquillacine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.taquillacine.databinding.ActivityCompraBinding
 
 class CompraActivity : AppCompatActivity() {
@@ -13,14 +14,26 @@ class CompraActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val bundle = intent.extras
+        val imagen = findViewById<ImageView>(R.id.imageView)
+
+        when(bundle!!.getString("pelicula")) {
+            "Gravedad","Gravity" -> imagen.setImageResource(R.drawable.gravity)
+            "Interestelar","Interestellar" -> imagen.setImageResource(R.drawable.interstellar)
+            "El Marciano","The Martian" -> imagen.setImageResource(R.drawable.the_martian)
+           // resources.getStringArray()
+        }
 
         var gracias = resources.getString(R.string.gracias)
-        binding.txtCompra.text = """¡¡$gracias!!
+        binding.txtCompra.text = """
+            |¡¡$gracias!!
             |
             |${resources.getString(R.string.pelicula)}: ${bundle!!.getString("pelicula")}
             |${resources.getString(R.string.sala)}: ${bundle!!.getString("sala")}
             |${resources.getString(R.string.horario)}: ${bundle!!.getString("horario")}
             |${resources.getString(R.string.noBoletos)}: ${bundle!!.getString("noBoletos")}
+            |
+            |
+            |${resources.getString(R.string.disfrute)}
         """.trimMargin()
     }
 //|Horario: ${datos?.get(2)}
