5eee6ac7981536bff8ac2337196a20b9331bd7fb
katearzate <katearzate000@gmail.com>
2021-04-09 22:13:14 -0700

    colores en botones actualizados

--- plataformaEscolar/app/src/main/java/com/example/plataformaescolar/MainActivity.kt
+++ plataformaEscolar/app/src/main/java/com/example/plataformaescolar/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.plataformaescolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.plataformaescolar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var usuario = ""
+    private var contrasena = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(LayoutInflater)
+        setContentView(binding.root)
+
     }
 }
