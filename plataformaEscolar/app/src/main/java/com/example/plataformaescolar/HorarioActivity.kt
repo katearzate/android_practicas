6488ff2dbe061ef2d2fdf97f931ef92e702ddc10
katearzate <katearzate000@gmail.com>
2021-05-02 15:01:35 -0500

    Spinner integrado en horario para los dias de la semana

--- plataformaEscolar/app/src/main/java/com/example/plataformaescolar/HorarioActivity.kt
+++ plataformaEscolar/app/src/main/java/com/example/plataformaescolar/HorarioActivity.kt
@@ -2,13 +2,18 @@ package com.example.plataformaescolar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import com.example.plataformaescolar.clases.AdapterMaterias
+import com.example.plataformaescolar.clases.Calificacion
 import com.example.plataformaescolar.clases.Materia
 import com.example.plataformaescolar.databinding.ActivityHorarioBinding
+import org.json.JSONObject
 
-class HorarioActivity : AppCompatActivity() {
+class HorarioActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
     private lateinit var  binding: ActivityHorarioBinding
+    private var diaDeLaSemana = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,47 +21,54 @@ class HorarioActivity : AppCompatActivity() {
         binding = ActivityHorarioBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.spinnerDiaDeLaSemana.onItemSelectedListener = this
+    }
 
-        val lista1 : MutableList<Materia> = mutableListOf()
-        lista1.add(Materia("08:00", "Administracion y Seguridad de Redes"))
-        lista1.add(Materia("10:00","Redes de Computadoras"))
-        lista1.add(Materia("13:00","Telecomunicaciones"))
-        lista1.add(Materia("14:00","Sistemas Operativos"))
-        lista1.add(Materia("15:00", "Introduccion a la Seguridad Informatica"))
-        lista1.add(Materia("17:00", "Desarrollo de Dispositivos Moviles"))
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+        val lista : MutableList<Materia> = mutableListOf()
+        diaDeLaSemana = resources.getStringArray(R.array.diasDeLaSemana)[p2]
 
-        val lista2 : MutableList<Materia> = mutableListOf()
-        lista2.add(Materia("08:00", "Administracion y Seguridad de Redes"))
-        lista2.add(Materia("09:00","Redes de Computadoras"))
-        lista2.add(Materia("13:00","Telecomunicaciones"))
-        lista2.add(Materia("14:00","Sistemas Operativos"))
-        lista2.add(Materia("17:00", "Desarrollo de Dispositivos Moviles"))
+        when(p2){
+            0-> {
 
-        val lista3 : MutableList<Materia> = mutableListOf()
-        lista3.add(Materia("10:00","Redes de computadoras"))
-        lista3.add(Materia("13:00","Telecomunicaciones"))
-        lista3.add(Materia("14:00","Sistemas operativos"))
-        lista3.add(Materia("15:00", "Introduccion a la Seguridad Informatica"))
+                lista.add(Materia("08:00", "Administracion y Seguridad de Redes"))
+                lista.add(Materia("10:00","Redes de Computadoras"))
+                lista.add(Materia("13:00","Telecomunicaciones"))
+                lista.add(Materia("14:00","Sistemas Operativos"))
+                lista.add(Materia("15:00", "Introduccion a la Seguridad Informatica"))
+                lista.add(Materia("17:00", "Desarrollo de Dispositivos Moviles"))
+                //binding.listViewDiaLaboral.adapter = AdapterMaterias(this, R.layout.lista_materias, lista)
+            }
+            1 -> {
+                lista.add(Materia("08:00", "Administracion y Seguridad de Redes"))
+                lista.add(Materia("09:00","Redes de Computadoras"))
+                lista.add(Materia("13:00","Telecomunicaciones"))
+                lista.add(Materia("14:00","Sistemas Operativos"))
+                lista.add(Materia("17:00", "Desarrollo de Dispositivos Moviles"))
+            }
+            2 -> {
+                lista.add(Materia("10:00","Redes de computadoras"))
+                lista.add(Materia("13:00","Telecomunicaciones"))
+                lista.add(Materia("14:00","Sistemas operativos"))
+                lista.add(Materia("15:00", "Introduccion a la Seguridad Informatica"))
+            }
+            3 -> {
+                lista.add(Materia("08:00", "Administracion y Seguridad de Redes"))
+                lista.add(Materia("09:00","Redes de Computadoras"))
+                lista.add(Materia("13:00","Telecomunicaciones"))
+                lista.add(Materia("14:00","Sistemas Operativos"))
+                lista.add(Materia("17:00", "Desarrollo de Dispositivos Moviles"))
+            }
+            4 -> {
+                lista.add(Materia("09:00","Redes de Computadoras"))
+                lista.add(Materia("13:00","Telecomunicaciones"))
+                lista.add(Materia("14:00","Sistemas Operativos"))
+            }
+        }
+        binding.listViewDiaLaboral.adapter = AdapterMaterias(this, R.layout.lista_materias, lista)
+    }
 
-        val lista4 : MutableList<Materia> = mutableListOf()
-        lista4.add(Materia("08:00", "Administracion y Seguridad de Redes"))
-        lista4.add(Materia("09:00","Redes de Computadoras"))
-        lista4.add(Materia("13:00","Telecomunicaciones"))
-        lista4.add(Materia("14:00","Sistemas Operativos"))
-        lista4.add(Materia("17:00", "Desarrollo de Dispositivos Moviles"))
-
-
-        val lista5 : MutableList<Materia> = mutableListOf()
-        lista5.add(Materia("09:00","Redes de Computadoras"))
-        lista5.add(Materia("13:00","Telecomunicaciones"))
-        lista5.add(Materia("14:00","Sistemas Operativos"))
-
-
-        binding.listViewLunes.adapter = AdapterMaterias(this, R.layout.lista_materias, lista1)
-        binding.listViewMartes.adapter = AdapterMaterias(this, R.layout.lista_materias, lista2)
-        binding.listViewMiercoles.adapter = AdapterMaterias(this, R.layout.lista_materias, lista3)
-        binding.listViewJueves.adapter = AdapterMaterias(this, R.layout.lista_materias, lista4)
-        binding.listViewViernes.adapter = AdapterMaterias(this, R.layout.lista_materias, lista5)
-
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
