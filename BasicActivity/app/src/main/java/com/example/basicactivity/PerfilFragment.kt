20770a230dbcce0e81d3076a580538e0676dbb99
katearzate <katearzate000@gmail.com>
2021-05-24 09:14:16 -0500

    diseño para fragmento de perfil y su recycler

--- BasicActivity/app/src/main/java/com/example/basicactivity/PerfilFragment.kt
+++ BasicActivity/app/src/main/java/com/example/basicactivity/PerfilFragment.kt
@@ -1,4 +1,21 @@
 package com.example.basicactivity
 
-class PerfilFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class PerfilFragment : Fragment(){
+    companion object {
+        fun newInstance(): PerfilFragment = PerfilFragment()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
