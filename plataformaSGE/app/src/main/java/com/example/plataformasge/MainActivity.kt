895fde0f5211d733afbb3d40c3d3966bbd7dca5c
katearzate <katearzate000@gmail.com>
2021-06-01 16:43:56 -0500

    bottom navigation menu definido

--- plataformaSGE/app/src/main/java/com/example/plataformasge/MainActivity.kt
+++ plataformaSGE/app/src/main/java/com/example/plataformasge/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.plataformasge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationMenu
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bottomNavMenu : BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fun onSupportNavigateUp(): Boolean {
+            onBackPressed()
+            return true
+        }
+
+        fun onOptionsItemSelected(item: MenuItem): Boolean {
+            when(item.itemId){
+                R.id.menuSalir -> finish()  //mandar a login
+                else -> true
+            }
+            return super.onOptionsItemSelected(item)
+        }
+        fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.menu_main, menu)
+            return true
+        }
     }
 }
