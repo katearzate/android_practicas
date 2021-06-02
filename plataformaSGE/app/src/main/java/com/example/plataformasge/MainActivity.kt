482e52687ba992635d308536da2342d5a53443a8
katearzate <katearzate000@gmail.com>
2021-06-02 01:39:33 -0500

    seccion de kardex agregada con navigationController

--- plataformaSGE/app/src/main/java/com/example/plataformasge/MainActivity.kt
+++ plataformaSGE/app/src/main/java/com/example/plataformasge/MainActivity.kt
@@ -5,18 +5,45 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.plataformasge.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationController : NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        navigationController = findNavController(R.id.main_container)
+        setupActionBarWithNavController(navigationController, AppBarConfiguration(setOf(
+            R.id.homeFragment, R.id.kardexFragment
+        )))
 
-
+        binding.menuBottomNavigation.setupWithNavController(navigationController)
+        binding.menuBottomNavigation.setOnNavigationItemSelectedListener { item ->
+            when(item.itemId){
+                R.id.menuInicio -> {
+                    navigationController.navigate(R.id.homeFragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.menuKardex -> {
+                    navigationController.navigate(R.id.kardexFragment)
+                    return@setOnNavigationItemSelectedListener true
+                }
+                else -> {
+                    //navigationController.navigate(R.id.homeFragment)
+                    return@setOnNavigationItemSelectedListener false
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
     }
 }
