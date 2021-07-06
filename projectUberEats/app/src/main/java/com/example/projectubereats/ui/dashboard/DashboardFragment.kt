4748b0caf4da1faba83e608d4148fc21057af1d5
katearzate <katearzate000@gmail.com>
2021-07-05 23:29:57 -0500

    fragments in navigation

---
+++ projectUberEats/app/src/main/java/com/example/projectubereats/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,22 @@
+package com.example.projectubereats.ui.dashboard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.projectubereats.R
+
+class DashboardFragment : Fragment() {
+
+    private var lat = 0.0
+    private var lng = 0.0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+}
