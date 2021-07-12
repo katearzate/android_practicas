d52577555a2b4d2d91671605d937d8da3ee77a72
katearzate <katearzate000@gmail.com>
2021-07-12 17:33:36 -0500

    favorite filter working

--- projectUberEats/app/src/main/java/com/example/projectubereats/ui/home/HomeFragment.kt
+++ projectUberEats/app/src/main/java/com/example/projectubereats/ui/home/HomeFragment.kt
@@ -8,12 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectubereats.R
 import com.example.projectubereats.models.Commerce
 import com.example.projectubereats.utils.Tools
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import mx.edu.itm.link.dadm_u3proyb.adapters.CommerceAdapter
 import org.json.JSONObject
 import java.lang.Exception
@@ -23,6 +25,9 @@ class HomeFragment : Fragment() {
     private lateinit var url : String
     lateinit var recyclerNegocios: RecyclerView
     lateinit var editSearch: EditText
+    lateinit var textFav: TextView
+    lateinit var btnFav: ExtendedFloatingActionButton
+    private var pressedFav: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,8 +35,8 @@ class HomeFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-
-
+        btnFav = view.findViewById(R.id.fabOnlyFav)
+        textFav = view.findViewById(R.id.textFavoritesShow)
         recyclerNegocios = view.findViewById(R.id.recyclerCommerces)
         editSearch = view.findViewById(R.id.editSearch)
 
@@ -48,6 +53,55 @@ class HomeFragment : Fragment() {
 
         })
 
+        val urlFav = "${resources.getString(R.string.api)}fav_comercios.php"
+        btnFav.setOnClickListener {
+
+            pressedFav = !pressedFav
+
+            if (pressedFav){
+                textFav.visibility = View.VISIBLE
+                object : Tools(){
+                    override fun formatResponse(response: String) {
+                        try {
+                            val json = JSONObject(response)
+                            val output = json.getJSONArray("output")
+
+                            val negocios = ArrayList<Commerce>()
+                            for(i in 0..output.length()-1) {
+                                val jsonCommerce = output.getJSONObject(i)
+                                val negocio = Commerce(
+                                    jsonCommerce.getInt("id_business"),
+                                    jsonCommerce.getString("business"),
+                                    jsonCommerce.getString("description"),
+                                    jsonCommerce.getString("address"),
+                                    jsonCommerce.getDouble("latitude"),
+                                    jsonCommerce.getDouble("longitude"),
+                                    jsonCommerce.getInt("id_category"),
+                                    jsonCommerce.getString("category"),
+                                    if(jsonCommerce.getInt("favorite")==1) true else false,
+                                    jsonCommerce.getString("photo")
+                                )
+                                negocios.add(negocio)
+                            }
+                            recyclerNegocios.layoutManager = LinearLayoutManager(requireContext())
+                            recyclerNegocios.adapter = CommerceAdapter(
+                                requireContext(),
+                                R.layout.recycler_row_commerce,
+                                negocios
+                            )
+
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                            "Error, no hay negocios disponibles".toast(requireContext())
+                        }
+                    }
+                }.consumeGet(requireContext(), urlFav)
+            }else{
+                textFav.visibility = View.INVISIBLE
+                refreshContacts()
+            }
+        }
+
         refreshContacts()
 
         return view
@@ -55,8 +109,6 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
     }
 
     fun refreshContacts(){
@@ -98,6 +150,8 @@ class HomeFragment : Fragment() {
                 }
             }
         }.consumeGet(requireContext(), url) //agregar params para la búsqueda
+
+
 
     }
 
